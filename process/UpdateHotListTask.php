ba1569a1b8af5e3684af344dbbfd2d2ea227acc0
lg <1720182606@qq.com>
2023-02-08 10:47:28 +0800

    修改端口 关闭redis插件

--- process/UpdateHotListTask.php
+++ process/UpdateHotListTask.php
@@ -10,29 +10,32 @@ use Workerman\Crontab\Crontab;
  */
 class UpdateHotListTask
 {
+    //错开时间执行，否则固定时间段接口会响应很慢
     public function onWorkerStart()
     {
-        // 每10分钟执行一次
-        new Crontab('0 */10 * * * *', function () {
+        // 每9分钟执行一次
+        new Crontab('0 */9 * * * *', function () {
             dump(date('Y-m-d H:i:s') . '更新头条开始');
             Event::emit('toutiao', null);
-
-            dump(date('Y-m-d H:i:s') . '更新天涯开始');
-            Event::emit('tianya', null);
-
-            dump(date('Y-m-d H:i:s') . '更新豆瓣开始');
-            Event::emit('douban', null);
-
-            dump(date('Y-m-d H:i:s') . '更新虎扑开始');
-            Event::emit('hupu', null);
 
             dump(date('Y-m-d H:i:s') . '更新微博开始');
             Event::emit('weibo', null);
         });
 
+        // 每10分钟执行一次
+        new Crontab('0 */10 * * * *', function () {
+            dump(date('Y-m-d H:i:s') . '更新天涯开始');
+            Event::emit('tianya', null);
 
-        // 每小时的第一分钟执行
-        new Crontab('0 1 * * * *', function () {
+            dump(date('Y-m-d H:i:s') . '更新豆瓣开始');
+            Event::emit('douban', null);
+        });
+
+        // 每11分钟执行一次
+        new Crontab('0 */11 * * * *', function () {
+            dump(date('Y-m-d H:i:s') . '更新虎扑开始');
+            Event::emit('hupu', null);
+
             dump(date('Y-m-d H:i:s') . '更新知乎热榜开始');
             //发布事件
             Event::emit('zhihu', null);
